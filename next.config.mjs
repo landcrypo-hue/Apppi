90ee57bce749d0200cc5fa71b752f90cfbc359a9
Mhtazi <landcrypo@gmail.com>
2026-09-25 16:29:59 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     unoptimized: true,
   },
   // Enable standalone output for Docker
-  output: 'standalone',
+  output: process.env.NETLIFY ? undefined : 'standalone',
   async headers() {
     return [
       {
